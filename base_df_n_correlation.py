f137c3438dbe2feca7cd9a104ae868484785bc8f
draichi <lucasdraichi@gmail.com>
2018-08-01 17:12:19 -0300

    cleaning up and refactoring

--- base_df_n_correlation.py
+++ base_df_n_correlation.py
@@ -6,14 +6,13 @@ def get_coin_data(coin):
     df = pd.read_csv('datasets/df_{}-{}-{}_{}-days.csv'.format(coin, todays_day, todays_month, days))
     return df
 
-base_df = get_coin_data('ethereum')
-
-for coin in coins:
+base_df = get_coin_data(coins[0])
+for coin in coins[1:]:
     df = get_coin_data(coin)
     base_df[coin] = df[coin]
 
 base_df.set_index('date', inplace=True)
 base_df.pct_change().corr(method='pearson')
-base_df.to_csv('datasets/base_df-{}-{}.csv'.format(todays_day, todays_month))
+base_df.to_csv('datasets/df_joined-{}-{}_{}-days.csv'.format(todays_day, todays_month, days))
 
-correlation_heatmap(base_df.pct_change(), "Correlation base_df")
+correlation_heatmap(base_df.pct_change(), '{} from {} days'.format(str(coins), days))
--- get_datasets.py
+++ get_datasets.py
@@ -1,19 +1,32 @@
-import threading, requests, time, os
-from configs.vars import coins, days, todays_month, todays_day
+import threading, requests, time, os, datetime
+from configs.vars import coins, days, todays_month, todays_day, keys
 import pandas as pd
+from termcolor import cprint
+
 start = time.time()
 
 def fetch_url(coin):
-    if not (os.path.exists('datasets/{}-{}-{}_{}-days.csv'.format(coin, todays_day, todays_month, days))):
-        print('-- downloading {}, {} days dataset, this will take a while'.format(coin, days))
+    if not (os.path.exists('datasets/df_{}-{}-{}_{}-days.csv'.format(coin, todays_day, todays_month, days))):
+        cprint('-- downloading {}, {} days dataset, this will take a while'.format(coin, days), 'yellow')
         url = "https://api.coingecko.com/api/v3/coins/{}/market_chart?vs_currency=btc&days={}".format(coin, days)
         headers = {'User-Agent': 'Mozilla/5.0'}
         response = requests.get(url, headers=headers)
         df = pd.DataFrame(response.json())
-        df.to_csv('datasets/{}-{}-{}_{}-days.csv'.format(coin, todays_day, todays_month, days), index=False)
-        print("---{} fetched and cached in {} seconds".format(coin, (time.time() - start)))
+        for key in keys:
+            for i, item in enumerate(df[key]):
+                str_item = str(item)
+                current_item = str_item.replace('[', '').replace(']', '').split(',')
+                date = current_item[0]
+                price = current_item[1]
+                dt = datetime.datetime.fromtimestamp(int(date)/1000).strftime('%Y-%m-%d %H:%M:%S')
+                df.loc[i, 'date'] = dt
+                df.loc[i, coin] = price
+        df = df[['date', coin]]
+        df.set_index('date', inplace=True)
+        df.to_csv('datasets/df_{}-{}-{}_{}-days.csv'.format(coin, todays_day, todays_month, days))
+        cprint("---{} fetched and cached in {} seconds".format(coin, (time.time() - start)), 'green')
     else:
-        print('-- loading {} from cache'.format(coin))
+        cprint('-- loading {} from cache'.format(coin), 'green')
 
 threads = [threading.Thread(target=fetch_url, args=(coin,)) for coin in coins]
 for thread in threads:
