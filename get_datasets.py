e62773d61854a3c417b410653dc187cb19aec0db
draichi <lucasdraichi@gmail.com>
2018-08-02 11:28:30 -0300

    improving print messages

--- get_datasets.py
+++ get_datasets.py
@@ -7,7 +7,7 @@ start = time.time()
 #------------------------------------------------------------->
 def fetch_url(coin):
     if not (os.path.exists('datasets/df_{}-{}-{}_{}-days.csv'.format(coin, todays_day, todays_month, days))):
-        cprint('-- downloading {}, {} days dataset, this will take a while'.format(coin, days), 'yellow')
+        cprint('> downloading {}, {} days dataset, this will take a while'.format(coin, days), 'yellow')
         url = "https://api.coingecko.com/api/v3/coins/{}/market_chart?vs_currency=btc&days={}".format(coin, days)
         headers = {'User-Agent': 'Mozilla/5.0'}
         response = requests.get(url, headers=headers)
@@ -24,9 +24,9 @@ def fetch_url(coin):
         df = df[['date', coin]]
         df.set_index('date', inplace=True)
         df.to_csv('datasets/df_{}-{}-{}_{}-days.csv'.format(coin, todays_day, todays_month, days))
-        cprint("---{} fetched and cached in {} seconds".format(coin, (time.time() - start)), 'green')
+        cprint("> {} fetched and cached in {} seconds".format(coin, (time.time() - start)), 'green')
     else:
-        cprint('-- loading {} from cache'.format(coin), 'green')
+        cprint('> loading {} from cache'.format(coin), 'green')
 #------------------------------------------------------------->
 threads = [threading.Thread(target=fetch_url, args=(coin,)) for coin in coins]
 for thread in threads:
