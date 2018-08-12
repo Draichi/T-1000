e3fb2dc62a6e3277866c4e02f9a30e1654d60ffe
draichi <lucasdraichi@gmail.com>
2018-08-11 22:04:15 -0300

    adding change points prior

--- configs/vars.py
+++ configs/vars.py
@@ -1,13 +1,12 @@
 import datetime
 #------------------------------------------------------------->
-# coins = [
-#     'steem','binancecoin','litecoin',
-#     'bitcoin', 'giant', 'steem-dollars','ethereum'
-# ]
+# customize these: 
 coins = ['bitcoin', 'litecoin']
-days = 90
-keys = ['prices', 'total_volumes', 'market_caps']
+days = 7
 currency = 'usd'
+changepoint_prior_scale = 0.15 # 0.10 ~ 0.50
+#------------------------------------------------------------->
+keys = ['prices', 'total_volumes', 'market_caps']
 todays_month = datetime.datetime.now().month
 todays_day = datetime.datetime.now().day
 #------------------------------------------------------------->
