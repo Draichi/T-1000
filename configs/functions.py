31e2ba318bc30751ccec9c3dff279e1d636e4695
draichi <lucasdraichi@gmail.com>
2018-08-14 02:47:39 -0300

    splitting code

---
+++ configs/functions.py
@@ -0,0 +1,112 @@
+import numpy as np
+import math, os, keras
+from termcolor import colored
+from keras.models import load_model
+from configs.agent import Agent
+from configs.vars import days, currency, todays_day, todays_month, terminal_width, batch_size
+#------------------------------------------------------------->
+width = os.get_terminal_size().columns
+#------------------------------------------------------------->
+# prints formatted price
+def format_price(n):
+	if n < 0:
+		return colored('Total profit: -{} {:.6f}'.format(currency.upper(), abs(n)), 'red', attrs=['bold'])
+	else:
+		return colored('Total profit: {} {:.7f}'.format(currency.upper(), abs(n)), 'green', attrs=['bold'])
+#------------------------------------------------------------->
+# returns the vector containing stock data from a fixed file
+def get_stock_data_vec(key):
+	vec = []
+	lines = open("datasets/" + key + ".csv", "r").read().splitlines()
+	for line in lines[1:]:
+		vec.append(float(line.split(",")[2]))# 1=marketcap, 2=prices, 3=vol
+	return vec
+#------------------------------------------------------------->
+# returns the sigmoid
+def _sigmoid(x):
+	try:
+		if x < 0:
+			return 1 - 1 / (1 + math.exp(x))
+		return 1 / (1 + math.exp(-x))
+	except OverflowError as err:
+		print("Overflow err: {0} - Val of x: {1}".format(err, x))
+	except ZeroDivisionError:
+		print("division by zero!")
+	except Exception as err:
+		print("Error in sigmoid: " + err)
+#------------------------------------------------------------->
+# returns an an n-day state representation ending at time t
+def get_state(data, t, n):
+	d = t - n + 1
+	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
+	res = []
+	for i in range(n - 1):
+		res.append(_sigmoid(block[i + 1] - block[i]))
+	return np.array([res])
+#------------------------------------------------------------->
+def operate(agent, asset_name, model_name=False, window_size=False):
+	if not model_name == False:
+		model = load_model("models/" + model_name)
+		window_size = model.layers[0].input.shape.as_list()[1]
+	asset = '{}-{}_{}_d{}_{}'.format(todays_day, todays_month, asset_name, days, currency)	
+	data = get_stock_data_vec(asset)
+	l = len(data) - 1
+	state = get_state(data, 0, window_size + 1)
+	total_profit = 0
+	agent.inventory = []
+	#------------------------------------------------------------->
+	print(colored('{}/{}'.format(asset_name.upper(), currency.upper()).center(terminal_width), 'cyan', attrs=['reverse','bold']))
+	print(colored('{} {:.7f} ~> {} {:.7f}'.format(currency.upper(),data[0], currency.upper(), data[-1]).center(terminal_width), 'cyan', attrs=['bold']))
+	print(colored('Model: {} - Window size: {}'.format(model_name, window_size).center(terminal_width), 'cyan', attrs=['bold']))
+	print(colored('Sample size: {} = {} days'.format(l, days).center(terminal_width), 'cyan', attrs=['underline','bold']))
+	#------------------------------------------------------------->
+	for t in range(l):
+		action = agent.act(state)
+		next_state = get_state(data, t + 1, window_size + 1)
+		reward = 0
+		print("> {} {} {:.7f}".format(t, currency.upper(),data[t]), end='\r') #hold
+		if action == 1: # buy
+			agent.inventory.append(data[t])
+			print(colored("> {} {} {:.7f} |".format(t, currency.upper(), data[t]), 'cyan'), format_price(total_profit), end='\r')
+		elif action == 2 and len(agent.inventory) > 0: # sell
+			bought_price = agent.inventory.pop(0)
+			reward = max(data[t] - bought_price, 0)
+			total_profit += data[t] - bought_price
+			print(colored("> {} {} {:.7f} |".format(t, currency.upper(), data[t]), 'yellow'), format_price(total_profit), end='\r')
+		done = True if t == l - 1 else False
+		agent.memory.append((state, action, reward, next_state, done))
+		state = next_state
+		if done:
+			print(colored("-----------------------------".center(terminal_width), 'cyan'))
+			print(format_price(total_profit).center(terminal_width))
+			print(colored("-----------------------------".center(terminal_width),'cyan'))
+		if len(agent.memory) > batch_size:
+			agent.expReplay(batch_size)
+#------------------------------------------------------------->
+def print_dollar():
+	print(chr(27) + "[2J")
+	print(colored("""
+   ||====================================================================||
+   ||//$\\\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//$\\\||
+   ||(100)==================| FEDERAL RESERVE NOTE |================(100)||
+   ||\\\$//        ~         '------========--------'                \\\$//||
+   ||<< /        /$\              // ____ \\\                         \ >>||
+   ||>>|  12    //L\\\            // ///..) \\\         L38036133B   12 |<<||
+   ||<<|        \\\ //           || <||  >\  ||                        |>>||
+   ||>>|         \$/            ||  $$ --/  ||        One Hundred     |<<||
+||====================================================================||>||
+||//$\\\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//$\\\||<||
+||(100)==================| FEDERAL RESERVE NOTE |================(100)||>||
+||\\\$//        ~         '------========--------'                \\\$//||\||
+||<< /        /$\              // ____ \\\                         \ >>||)||
+||>>|  12    //L\\\            // ///..) \\\         L38036133B   12 |<<||/||
+||<<|        \\\ //           || <||  >\  ||                        |>>||=||
+||>>|         \$/            ||  $$ --/  ||        One Hundred     |<<||
+||<<|      L38036133B        *\\\  |\_/  //* series                 |>>||
+||>>|  12                     *\\\/___\_//*   1989                  |<<||
+||<<\      Treasurer     ______/Franklin\________     Secretary 12 />>||
+||//$\                 ~|UNITED STATES OF AMERICA|~               /$\\\||
+||(100)===================  ONE HUNDRED DOLLARS =================(100)||
+||\\\$//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\\$//||
+||====================================================================||           
+	""", 'green'))
--- configs/vars.py
+++ configs/vars.py
@@ -1,12 +1,15 @@
-import datetime
+import datetime, os
 #------------------------------------------------------------->
 # customize these: 
-coins = ['bitcoin', 'litecoin']
-days = 20
-currency = 'usd'
+coins = ['nano', 'binancecoin', 'monero', 'zcash']
+days = 90
+currency = 'btc'
 changepoint_prior_scale = 0.15 # 0.10 ~ 0.50
 #------------------------------------------------------------->
-keys = ['prices', 'total_volumes', 'market_caps']
+keys = ['prices', 'total_volumes', 'market_caps'] # if change that, will have to change the getDataVec in function.py
 todays_month = datetime.datetime.now().month
 todays_day = datetime.datetime.now().day
+terminal_width = os.get_terminal_size().columns
+batch_size = 32
+
 #------------------------------------------------------------->
