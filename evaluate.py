e73041920e2b9fc8cfd4923d10f8b4341a0fbfc3
draichi <lucasdraichi@gmail.com>
2018-11-14 12:20:55 -0200

    refactoring evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -12,14 +12,19 @@ from configs.vars import days, currency, todays_day, todays_month, terminal_widt
 asset_name, model_name = sys.argv[1], sys.argv[2]
 model = load_model("models/" + model_name)
 window_size = model.layers[0].input.shape.as_list()[1]
+agent = Agent(window_size, True, model_name)
 #------------------------------------------------------------->
 print(chr(27) + "[2J")
 #------------------------------------------------------------->
-agent = Agent(window_size, True, model_name)
+div()
+print(colored('{}/{}'.format(asset_name.upper(), currency.upper()).center(terminal_width), 'white', attrs=['bold']))
+print(colored('MODEL: {}'.format(model_name).center(terminal_width), 'white', attrs=['bold']))
+div()
+#------------------------------------------------------------->
 operate(
 	agent=agent,
 	asset_name=asset_name,
 	window_size=window_size,
-	model_name=model_name
-)
+	model_name=model_name)
+#------------------------------------------------------------->
 print(colored('D O N E'.center(terminal_width),'white','on_green',attrs=['bold']))
