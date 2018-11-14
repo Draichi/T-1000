d00d6d5be29207596d038acf05d855a5110cff67
draichi <lucasdraichi@gmail.com>
2018-11-14 12:20:33 -0200

    refactoring train.py

--- train.py
+++ train.py
@@ -14,15 +14,17 @@ agent = Agent(window_size)
 print(chr(27) + "[2J")
 #------------------------------------------------------------->
 for e in range(episode_count+1):
-    print('\n')
-    print(colored('~'.center(terminal_width),'cyan'))
-    print(colored("EPISODE {}/{}:".format(str(e),str(episode_count)).center(terminal_width),'magenta',attrs=['bold']))
-    operate(
-        agent=agent,
-        asset_name=asset_name,
-        model_name=False,
-        window_size=window_size
-    )
-    # if e % 50 == 0:
-    #     agent.model.save("models/{}-{}_{}_d{}_e{}_w{}_c{}_{}_b{}".format(todays_day,todays_month,asset_name,days,str(e),window_size,episode_count,currency,batch_size))
+	print('\n')
+	print(colored('~'.center(terminal_width),'cyan'))
+	print(colored('{}/{}'.format(asset_name.upper(), currency.upper()).center(terminal_width), 'white', attrs=['bold']))
+	div()
+	print(colored("EPISODE {}/{}:".format(str(e),str(episode_count)).center(terminal_width),'magenta',attrs=['bold']))
+	div()    
+	#------------------------------------------------------------->
+	operate(
+		agent=agent,
+		asset_name=asset_name,
+		model_name=False,
+		window_size=window_size)
+#------------------------------------------------------------->
 print('\n\n',colored('D O N E'.center(terminal_width),'white','on_green',attrs=['bold']))
