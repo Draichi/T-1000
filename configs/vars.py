d3a00c30abeeb88fe4c2378c23a12d04d2fa0bdb
draichi <lucasdraichi@gmail.com>
2018-11-14 12:20:00 -0200

    adding epochs as a variable

--- configs/vars.py
+++ configs/vars.py
@@ -3,13 +3,15 @@ import datetime, os
 coins = ['bitcoin', 'nano', 'ripple']
 # coins = ['binancecoin', 'nano', 'ripple']
 # coins= ['bitcoin','giant','ethereum-classic', 'binancecoin', 'litecoin', 'ethereum', 'bitshares', 'steem','dash','nano','zcash','eos','neo','blocknet']
-days = '30'
+days = '14'
 currency = 'usd'
-batch_size = 512 # 16, 32, 64, 128, 256...
 
-wallet = 8000
+wallet = 8000.00
 n_orders = 350
 fees = 1
+
+epochs = 10
+batch_size = 8 # 4, 8, 16, 32...
 gamma = 0.95
 epsilon = 1.0
 epsilon_min = 0.01
