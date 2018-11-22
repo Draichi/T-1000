73f36ff82b2e467acdbc7113c437406baa66c1b1
draichi <lucasdraichi@gmail.com>
2018-11-21 22:54:03 -0200

    updating vars

--- configs/vars.py
+++ configs/vars.py
@@ -7,16 +7,17 @@ days = '14'
 currency = 'usd'
 
 wallet = 3000.00
-n_orders = 350
+n_orders = 1000
 fees = 1
 
 epochs = 1
-batch_size = 128 # 4, 8, 16, 32...
+batch_size = 64 # 4, 8, 16, 32...
 gamma = 0.95
 epsilon = 0.1
 epsilon_min = 0.01
 epsilon_decay = 0.995
 #------------------------------------------------------------->
+n_features = 3 # price, caps, vol
 keys = ['prices', 'market_caps', 'total_volumes'] # if change this, change configs/function.py:22
 todays_month = datetime.datetime.now().month
 todays_day = datetime.datetime.now().day
