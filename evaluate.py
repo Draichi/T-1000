93de691321248ef31d3010488227014c2a1db8d3
draichi <lucasdraichi@gmail.com>
2019-03-10 14:02:06 -0300

    just organizing code

--- evaluate.py
+++ evaluate.py
@@ -8,23 +8,23 @@ from keras.models import load_model
 from configs.agent import Agent
 from configs.functions import *
 from configs.vars import days, currency, todays_day, todays_month, terminal_width, batch_size
-#------------------------------------------------------------->
+#---------------------------------------------------------------------------------->
 asset_name, model_name = sys.argv[1], sys.argv[2]
 model = load_model("models/" + model_name)
 window_size = model.layers[0].input.shape.as_list()[1]
 agent = Agent(window_size, True, model_name)
-#------------------------------------------------------------->
+#---------------------------------------------------------------------------------->
 print(chr(27) + "[2J")
-#------------------------------------------------------------->
+#---------------------------------------------------------------------------------->
 div()
 print(colored('{}/{}'.format(asset_name.upper(), currency.upper()).center(terminal_width), 'white', attrs=['bold']))
 print(colored('MODEL: {}'.format(model_name).center(terminal_width), 'white', attrs=['bold']))
 div()
-#------------------------------------------------------------->
+#---------------------------------------------------------------------------------->
 operate(
 	agent=agent,
 	asset_name=asset_name,
 	window_size=window_size,
 	model_name=model_name)
-#------------------------------------------------------------->
+#---------------------------------------------------------------------------------->
 print(colored('D O N E'.center(terminal_width),'white','on_green',attrs=['bold']))
