290c66ba90cee8bf24f7177873cc6473203bdbec
draichi <lucasdraichi@gmail.com>
2019-03-16 18:29:37 -0300

    wip

--- configs/vars.py
+++ configs/vars.py
@@ -1,21 +1,24 @@
 import datetime, os
 #---------------------------------------------------------------------------------->
 ####### PORTFOLIO: #########
-coins = [
-    'ethereum-classic', 'binancecoin', 'litecoin', 'ethereum',
-    # 'dash', 
-    'nano', 
-    # 'zcash', 
-    'eos',
-    #  'neo', 
-    # 'theta-token',
-    # 'stellar', 'ripple', 'tron', 'icon', 
-    'cardano', 
-    # 'nexus',
-    # 'ravencoin', 'etherparty', '0x', 'omisego'
+PORTFOLIO_SYMBOLS = ['btc', 'eth', 'eos', 'ada']
+
+# https://api.datalight.me/v1/object/
+FIELDS = [ 
+    'coinmarketcap.coin_btc.price_btc',
+    'telegram.coin_hype_mood_daily.mood',
+    'twitter.hype.twitter_hype',
+    'ethereum_go.transaction_stat_usd_24h.dl_block_ind',
+    'ethereum_go.transaction_calc.trxns_sum',
+    'current.coin.rate_yearly',
+    'ethereum_go.transaction_calc.trxns_count',
+    'coinmarketcap.coin_cvix_30d_1h.cvix',
+    'coinmarketcap.coin_calc.liquidity',
+    'coinmarketcap.coin.price_usd'
 ]
-days = '120'
-currency = 'btc'
+TIME_INTERVAL = '1d'
+FROM_DATE = '2018-11-01'
+TO_DATE = '2019-01-04'
 #---------------------------------------------------------------------------------->
 ####### MARKET PARAMETERS: #########
 wallet = 3000.00
@@ -31,12 +34,14 @@ epsilon_min = 0.01
 epsilon_decay = 0.995
 #---------------------------------------------------------------------------------->
 ####### CONSTANTS (DO NOT CHANGE) #########
+STR_PORTFOLIO_SYMBOLS = ','.join(PORTFOLIO_SYMBOLS)
+STR_FIELDS = ','.join(FIELDS)
 n_features = 3 # price, caps, vol
 keys = ['prices', 'market_caps', 'total_volumes'] # if change this, change configs/function.py:22
 todays_month = datetime.datetime.now().month
 todays_day = datetime.datetime.now().day
 terminal_width = os.get_terminal_size().columns
-PATH_TO_CORRELATION_FILE = 'datasets/' + str(todays_day) + '-' + str(todays_month) + '_correlation_' + str(days) + '_' + currency + '.csv'
-PATH_TO_WEIGHTS_FILE = 'datasets/' + str(todays_day) + '-' + str(todays_month) + '_weighs_' + str(days) + '_' + currency + '.csv'
-PATH_TO_PCT_CORRELATION_FILE = 'datasets/' + str(todays_day) + '-' + str(todays_month) + '_pct_correlation_' + str(days) + '_' + currency + '.csv'
-PATH_TO_COIN_FILE = ['datasets/' + str(todays_day) + '-' + str(todays_month) + '_' + coin + '_'+str(days) + '_' + currency + '.csv' for coin in coins]
+# PATH_TO_CORRELATION_FILE = 'datasets/' + str(todays_day) + '-' + str(todays_month) + '_correlation_' + str(days) + '_' + currency + '.csv'
+# PATH_TO_WEIGHTS_FILE = 'datasets/' + str(todays_day) + '-' + str(todays_month) + '_weighs_' + str(days) + '_' + currency + '.csv'
+# PATH_TO_PCT_CORRELATION_FILE = 'datasets/' + str(todays_day) + '-' + str(todays_month) + '_pct_correlation_' + str(days) + '_' + currency + '.csv'
+# PATH_TO_COIN_FILE = ['datasets/' + str(todays_day) + '-' + str(todays_month) + '_' + coin + '_'+str(days) + '_' + currency + '.csv' for coin in coins]
