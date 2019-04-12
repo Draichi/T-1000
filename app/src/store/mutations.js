7623beba9c63eb71255e6ff71988aa7477081f2d
draichi <lucasdraichi@gmail.com>
2019-04-12 18:04:25 -0300

    wip

--- app/src/store/mutations.js
+++ app/src/store/mutations.js
@@ -26,7 +26,8 @@ export default {
             // 'headers': {'Content-Encoding': 'gzip', 'Access-Control-Allow-Origin': '*'},
             'dataset': {'ds': coins[key].data.labels, 'y': coins[key].data.series[0]},
             'changepoint_prior_scale': payload.changepoint,
-            'forecast_days': payload.forecast
+            'forecast_days': payload.forecast,
+            'symbol': payload.symbol
           })
           .then(res => console.log(res))
       }
