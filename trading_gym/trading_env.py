2fed29c8499d97363eea5805a17529d5390a62ea
draichi <lucasdraichi@gmail.com>
2019-04-17 00:04:55 -0300

    implementing render method with matplotlib

--- trading_gym/trading_env.py
+++ trading_gym/trading_env.py
@@ -3,11 +3,22 @@ Classic cart-pole system implemented by Rich Sutton et al.
 Copied from http://incompleteideas.net/sutton/book/code/pole.c
 permalink: https://perma.cc/C9ZM-652R
 """
-
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import gym
 from gym.spaces import Discrete, Box
+
+plt.style.use('dark_background')
+mpl.rcParams.update(
+    {
+        "font.size": 15,
+        "axes.labelsize": 15,
+        "lines.linewidth": 1,
+        "lines.markersize": 8
+    }
+)
 
 def init_data():
     df = pd.read_csv('/home/lucas/Documents/cryptocurrency_prediction/datasets/LTC_1d_2018-11-01_2019-03-18.csv')
@@ -28,36 +39,18 @@ def init_data():
     return keys, df_array
 
 class TradingEnv(gym.Env):
-    """Example of a custom env in which you have to walk down a corridor.
-
-     Observation:
-        Type: Box(4)
-        Num	Observation                 Min         Max
-        0	open price                -4.8            4.8
-        1	close price               -Inf            Inf
-        2	Momentum                  -24 deg        24 deg
-        3	Volume                    -Inf            Inf
-    Actions:
-        Type: Discrete(3)
-        Num	Action
-        0	Buy
-        1	Sell
-        2   Hold
-        TODO
-        - buy or sell a proportion of the avaiable on wallet (20%, 30%...)
-
-     Reward:
-        Reward is the natural log of the wallet difference between before action and after action
-    """
 
     def __init__(self):
         # self.keys, self.symbol_list = init_data(FLAGS.symbol)
         self.keys, self.symbol_list = init_data()
         self.index = 0
+        self._first_render = True
+        self._spread_coefficients = [1]
         self.dicount_rate = 0.999 # works like the tx
         self.wallet_btc = 1.0
         self.wallet_symbol = 0.0
         self.action_space = Discrete(3)
+        self.action = 3
         self.observation_space = Box(
             low=-np.finfo(np.float32).max, high=np.finfo(np.float32).max,
             shape=(len(self.keys), ), dtype=np.float32
@@ -78,7 +71,7 @@ class TradingEnv(gym.Env):
     def step(self, action):
         if action not in [0,1,2]:
             raise AssertionError()
-
+        self.action = action
         price_btc_index = list(self.keys).index('Price BTC')
         wallet_btc_index = list(self.keys).index('wallet_btc')
         wallet_symbol_index = list(self.keys).index('wallet_symbol')
@@ -113,3 +106,60 @@ class TradingEnv(gym.Env):
 
         # return normalized_next_state, reward, done, {}
         return next_observable_state, reward, done, {}
+
+    def _handle_close(self, evt):
+        self._closed_plot = True
+
+    def render(self, savefig=False, filename='myfig'):
+
+        if self._first_render:
+            self._f, self._ax = plt.subplots(
+                len(self._spread_coefficients) + int(len(self._spread_coefficients) > 1),
+                sharex=True
+            )
+            if len(self._spread_coefficients) == 1:
+                self._ax = [self._ax]
+            self._f.set_size_inches(12, 6)
+            self._first_render = False
+            self._f.canvas.mpl_connect('close_event', self._handle_close)
+        if len(self._spread_coefficients) > 1:
+            # TODO: To be checked
+            for prod_i in range(len(self._spread_coefficients)):
+                bid = self._prices_history[-1][2 * prod_i]
+                ask = self._prices_history[-1][2 * prod_i + 1]
+                self._ax[prod_i].plot([self.index, self.index + 1],
+                                      [bid, bid], color='white')
+                self._ax[prod_i].plot([self.index, self.index + 1],
+                                      [ask, ask], color='white')
+                self._ax[prod_i].set_title('Product {} (spread coef {})'.format(
+                    prod_i, str(self._spread_coefficients[prod_i])))
+
+        price_btc_index = list(self.keys).index('Price BTC')
+        observable_state = self.symbol_list[self.index]
+        price_btc_before_action = observable_state[price_btc_index]
+        # Spread price
+        prices = price_btc_before_action
+        # bid, ask = calc_spread(prices, self._spread_coefficients)
+        self._ax[-1].plot([self.index, self.index + 1],
+                          [prices, prices], color='white')
+        # self._ax[-1].plot([self.index, self.index + 1],
+        #                   [prices, prices], color='white')
+        ymin, ymax = self._ax[-1].get_ylim()
+        yrange = ymax - ymin
+        if self.action == 1: #sell
+            self._ax[-1].scatter(self.index + 0.5, prices + 0.03 *
+                                 yrange, color='orangered', marker='v')
+        elif self.action == 0: #buy
+            self._ax[-1].scatter(self.index + 0.5, prices - 0.03 *
+                                 yrange, color='lawngreen', marker='^')
+        # plt.suptitle('Cumulated Reward: ' + "%.2f" % self._total_reward + ' ~ ' +
+        #              'Cumulated PnL: ' + "%.2f" % self._total_pnl + ' ~ ' +
+        #              'Position: ' + ['flat', 'long', 'short'][list(self._position).index(1)] + ' ~ ' +
+        #              'Entry Price: ' + "%.2f" % self._entry_price)
+        self._f.tight_layout()
+        plt.xticks(range(self.index)[::5])
+        plt.xlim([max(0, self.index - 80.5), self.index + 0.5])
+        plt.subplots_adjust(top=0.85)
+        plt.pause(0.01)
+        if savefig:
+            plt.savefig(filename)
