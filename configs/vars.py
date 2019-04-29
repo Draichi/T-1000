524259ee605d8503057320392084f25ac7a7d1ee
draichi <lucasdraichi@gmail.com>
2019-04-29 17:40:49 -0300

    updating vars

--- configs/vars.py
+++ configs/vars.py
@@ -3,8 +3,8 @@ import os
 import numpy as np
 #---------------------------------------------------------------------------------->
 ####### MARKET PARAMETERS: #########
-WALLET_BTC = 100.0
-WALLET_SYMBOL = 100.0
+WALLET_BTC = 1.0
+WALLET_SYMBOL = 1.0
 FEES = 0.002 # 0.2%
 #---------------------------------------------------------------------------------->
 TODAYS_MONTH = datetime.datetime.now().month
