3d2e9d551a288657669e70dab7033bbd0b5e1a5a
draichi <lucasdraichi@gmail.com>
2019-05-14 03:04:26 -0300

    removing old files

--- app/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  devServer: {
-    disableHostCheck: true
-  },
-  outputDir: '../docs/',
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/cryptocurrency_prediction/'
-    : '/'
-}
