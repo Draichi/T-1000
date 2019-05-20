30dcc2b187bce5cbd95ba0cf88c19b64396ffd2c
draichi <lucasdraichi@gmail.com>
2019-05-20 07:37:58 -0300

    updating and cleaning functions.py

--- configs/functions.py
+++ configs/functions.py
@@ -11,9 +11,7 @@ import colorama
 import requests
 import pandas as pd
 import numpy as np
-# import plotly.graph_objs as go
 from termcolor import colored
-from configs.vars import WALLET_FIRST_SYMBOL, WALLET_SECOND_SYMBOL
 
 colorama.init()
 
@@ -111,11 +109,6 @@ def get_datasets(symbol, to_symbol, histo, limit):
         df.loc[:, 'AD'] = talib.AD(high, low, close, volume)
         df.loc[:, 'ADOSC'] = talib.ADOSC(high, low, close, volume, fastperiod=3, slowperiod=10)
         df.loc[:, 'OBV'] = talib.OBV(close, volume)
-        # wallet indicator to trading bot
-        # df.loc[:, 'wallet_{}'.format(symbol)] = 1.0
-        df.loc[:, 'wallet_first_symbol'] = WALLET_FIRST_SYMBOL
-        df.loc[:, 'wallet_second_symbol'] = WALLET_SECOND_SYMBOL
-        # df.loc[:, 'wallet_{}'.format(to_symbol)] = 0.0
 
         # df.fillna(df.mean(), inplace=True)
         df.dropna(inplace=True)
@@ -135,77 +128,6 @@ def get_datasets(symbol, to_symbol, histo, limit):
     return df_train, df_rollout
 
 #------------------------------------------------------------->
-
-# def init_data(pair, mode, limit, histo):
-#     """Tranform the data from pandas.DataFrame to list to improve Ray's performance
-
-#     Arguments:
-#         pair {str} -- Pair
-#         mode {str ['train', 'rollout]} -- Select the correct dataset,
-#         train or rollout one.
-
-#     Returns:
-#         list, list -- The dataframe divided in two lists
-#     """
-#     df = pd.read_csv('datasets/bot_{}_{}_{}_{}.csv'.format(mode, pair, limit, histo))
-#     df.drop('Date', axis=1, inplace=True)
-#     df_array = df.values.tolist()
-#     keys = df.keys()
-#     return keys, df_array
-
-
-# def build_layout(title, x_axis_title, y_axis_title):
-#     """Create the plotly's layout with custom configuration
-
-#     Arguments:
-#         title {str} -- Layout's central title
-#         x_axis_title {str} -- Axis x title
-#         y_axis_title {str} -- Axis y title
-
-#     Returns:
-#         Object -- Plotly object from plotly.graph_objs
-#     """
-
-#     layout = go.Layout(plot_bgcolor='#2d2929',
-#                        paper_bgcolor='#2d2929',
-#                        title=title,
-#                        font=dict(color='rgb(255, 255, 255)', size=17),
-#                        legend=dict(orientation="h"),
-#                        yaxis=dict(title=y_axis_title),
-#                        xaxis=dict(title=x_axis_title))
-#     return layout
-
-# def var_cov_matrix(df, weigths):
-#     """Compute covariance matrix with respect of given weigths
-
-#     Arguments:
-#         df {pandas.DataFrame} -- The timeseries object
-#         weigths {list} -- List of weights to be used
-
-#     Returns:
-#         numpy.array -- The covariance matrix
-#     """
-
-#     sigma = np.cov(np.array(df).T, ddof=0)
-#     var = (np.array(weigths) * sigma * np.array(weigths).T).sum()
-#     return var
-
-# def calc_exp_returns(avg_return, weigths):
-#     """Compute the expected returns
-
-#     Arguments:
-#         avg_return {pandas.DataFrame} -- The average of returns
-#         weigths {list} -- A list of weigths
-
-#     Returns:
-#         array -- N dimensions array
-#     """
-
-#     exp_returns = avg_return.dot(weigths.T)
-#     return exp_returns
-
-
-
 
 def print_dollar():
     print(chr(27) + "[2J")
