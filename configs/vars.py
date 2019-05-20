c1c89902f39f0964fed6f9b32c34328b771842b0
draichi <lucasdraichi@gmail.com>
2019-05-20 07:37:38 -0300

    updating vars

--- configs/vars.py
+++ configs/vars.py
@@ -1,11 +1,21 @@
-import datetime
-import os
-import numpy as np
-#---------------------------------------------------------------------------------->
-####### MARKET PARAMETERS: #########
-WALLET_FIRST_SYMBOL = 10
-WALLET_SECOND_SYMBOL = 1
-FEES = 0.002 # 0.2%
-#---------------------------------------------------------------------------------->
-TODAYS_MONTH = datetime.datetime.now().month
-TODAYS_DAY = datetime.datetime.now().day
+############ ENV VARIABLES ################
+INITIAL_ACCOUNT_BALANCE = 10000
+LOOKBACK_WINDOW_SIZE = 40
+
+############ RENDER VARIABLES ################
+VOLUME_CHART_HEIGHT = 0.33
+
+# style 1
+# UP_COLOR = '#297fff'
+# DOWN_COLOR = '#ffaa00'
+# UP_TEXT_COLOR = '#297fff'
+# DOWN_TEXT_COLOR = '#ffaa00'
+
+# style 2
+UP_COLOR = '#00b909'
+DOWN_COLOR = '#c60606'
+UP_TEXT_COLOR = '#00b909'
+DOWN_TEXT_COLOR = '#c60606'
+
+BOT_COLOR = '#ffffff'
+BUY_N_HOLD_COLOR = '#dcaba2'
