43619be18ec5db1540798766dcc9a7eac6dfaaf7
draichi <lucasdraichi@gmail.com>
2019-05-23 05:17:04 -0300

    hot fix mistyping

--- env/MultiModelRender.py
+++ env/MultiModelRender.py
@@ -38,18 +38,18 @@ class StockTradingGraph:
         # https://matplotlib.org/users/gridspec.html
         # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.subplot2grid.html?highlight=subplot2grid#matplotlib.pyplot.subplot2grid
         self.net_worth_ax = plt.subplot2grid(
-            (6, 1), (0, 0), rowspan=2, colspan=1)
+            (8, 1), (0, 0), rowspan=2, colspan=1)
 
         self.price_ax1 = plt.subplot2grid(
-            (6, 1), (2, 0), rowspan=2, colspan=1, sharex=self.net_worth_ax)
+            (8, 1), (2, 0), rowspan=2, colspan=1, sharex=self.net_worth_ax)
         self.volume_ax1 = self.price_ax1.twinx()
 
         self.price_ax2 = plt.subplot2grid(
-            (6, 1), (4, 0), rowspan=2, colspan=1, sharex=self.net_worth_ax)
+            (8, 1), (4, 0), rowspan=2, colspan=1, sharex=self.net_worth_ax)
         self.volume_ax2 = self.price_ax2.twinx()
 
         self.price_ax3 = plt.subplot2grid(
-            (6, 1), (6, 0), rowspan=2, colspan=1, sharex=self.net_worth_ax)
+            (8, 1), (6, 0), rowspan=2, colspan=1, sharex=self.net_worth_ax)
         self.volume_ax3 = self.price_ax3.twinx()
 
         # Add padding to make graph easier to view
@@ -63,8 +63,8 @@ class StockTradingGraph:
         # Clear the frame rendered last step
         self.net_worth_ax.clear()
         # set balance and shares held
-        first_coin, self.second_coin = self.render_title.split('/')
-        self.net_worth_ax.set_title('Holding {:.3f} {} and {:.3f} {}'.format(shares_held, first_coin, balance, self.second_coin))
+        first_coin, self.second_coin = 'um', 'dois'
+        self.net_worth_ax.set_title('Holding {:.3f} {} and {:.3f} {}'.format(shares_held1, first_coin, balance, self.second_coin))
         # compute performance
         abs_diff = net_worth - buy_and_hold
         avg = (net_worth + buy_and_hold) / 2
@@ -83,7 +83,7 @@ class StockTradingGraph:
         legend = self.net_worth_ax.legend(loc=2, ncol=2, prop={'size': 8})
         legend.get_frame().set_alpha(0.4)
 
-        last_date = date2num(self.df['Date'].values[current_step])
+        last_date = date2num(self.df1['Date'].values[current_step])
         last_net_worth = self.net_worths[current_step]
         last_buy_and_hold = self.buy_and_holds[current_step]
 
@@ -108,60 +108,118 @@ class StockTradingGraph:
         #     min(self.buy_and_holds[np.nonzero(self.buy_and_holds)]) / 1.25, max(self.net_worths) * 1.25)
 
     def _render_price(self, current_step, net_worth, dates, step_range):
-        self.price_ax.clear()
+        self.price_ax1.clear()
+        self.price_ax2.clear()
+        self.price_ax3.clear()
 
-        # Format data for OHCL candlestick graph
-        candlesticks = zip(dates,
-                           self.df['open'].values[step_range], self.df['close'].values[step_range],
-                           self.df['high'].values[step_range], self.df['low'].values[step_range])
+        candlesticks1 = zip(dates,
+                           self.df1['open'].values[step_range], self.df1['close'].values[step_range],
+                           self.df1['high'].values[step_range], self.df1['low'].values[step_range])
+        candlesticks2 = zip(dates,
+                           self.df2['open'].values[step_range], self.df2['close'].values[step_range],
+                           self.df2['high'].values[step_range], self.df2['low'].values[step_range])
+        candlesticks3 = zip(dates,
+                           self.df3['open'].values[step_range], self.df3['close'].values[step_range],
+                           self.df3['high'].values[step_range], self.df3['low'].values[step_range])
 
         # Plot price using candlestick graph from mpl_finance
-        candlestick(self.price_ax, candlesticks, width=.04,
+        candlestick(self.price_ax1, candlesticks1, width=.04,
+                    colorup=UP_COLOR, colordown=DOWN_COLOR)
+        candlestick(self.price_ax2, candlesticks2, width=.04,
+                    colorup=UP_COLOR, colordown=DOWN_COLOR)
+        candlestick(self.price_ax3, candlesticks3, width=.04,
                     colorup=UP_COLOR, colordown=DOWN_COLOR)
 
-        last_date = date2num(self.df['Date'].values[current_step])
-        last_close = self.df['close'].values[current_step]
-        last_high = self.df['high'].values[current_step]
+        last_date = date2num(self.df1['Date'].values[current_step])
+        last_close1 = self.df1['close'].values[current_step]
+        last_high1 = self.df1['high'].values[current_step]
+        last_close2 = self.df2['close'].values[current_step]
+        last_high2 = self.df2['high'].values[current_step]
+        last_close3 = self.df3['close'].values[current_step]
+        last_high3 = self.df3['high'].values[current_step]
 
         # Print the current price to the price axis
-        self.price_ax.annotate('{0:.4f}'.format(last_close), (last_date, last_close),
-                               xytext=(last_date, last_high),
+        self.price_ax1.annotate('{0:.4f}'.format(last_close1), (last_date, last_close1),
+                               xytext=(last_date, last_high1),
+                               bbox=dict(boxstyle='round',
+                                         fc='w', ec='k', lw=1),
+                               color="black",
+                               fontsize="small")
+        self.price_ax2.annotate('{0:.4f}'.format(last_close2), (last_date, last_close2),
+                               xytext=(last_date, last_high2),
+                               bbox=dict(boxstyle='round',
+                                         fc='w', ec='k', lw=1),
+                               color="black",
+                               fontsize="small")
+        self.price_ax3.annotate('{0:.4f}'.format(last_close3), (last_date, last_close3),
+                               xytext=(last_date, last_high3),
                                bbox=dict(boxstyle='round',
                                          fc='w', ec='k', lw=1),
                                color="black",
                                fontsize="small")
 
         # Shift price axis up to give volume chart space
-        ylim = self.price_ax.get_ylim()
-        self.price_ax.set_ylim(ylim[0] - (ylim[1] - ylim[0])
-                               * VOLUME_CHART_HEIGHT, ylim[1])
+        ylim1 = self.price_ax1.get_ylim()
+        ylim2 = self.price_ax2.get_ylim()
+        ylim3 = self.price_ax3.get_ylim()
+
+        self.price_ax1.set_ylim(ylim1[0] - (ylim1[1] - ylim1[0])
+                               * VOLUME_CHART_HEIGHT, ylim1[1])
+        self.price_ax2.set_ylim(ylim2[0] - (ylim2[1] - ylim2[0])
+                               * VOLUME_CHART_HEIGHT, ylim2[1])
+        self.price_ax3.set_ylim(ylim3[0] - (ylim3[1] - ylim3[0])
+                               * VOLUME_CHART_HEIGHT, ylim3[1])
 
     def _render_volume(self, current_step, net_worth, dates, step_range):
-        self.volume_ax.clear()
+        self.volume_ax1.clear()
+        self.volume_ax2.clear()
+        self.volume_ax3.clear()
 
-        volume = np.array(self.df['volumefrom'].values[step_range])
+        volume1 = np.array(self.df1['volumefrom'].values[step_range])
+        volume2 = np.array(self.df2['volumefrom'].values[step_range])
+        volume3 = np.array(self.df3['volumefrom'].values[step_range])
 
-        pos = self.df['open'].values[step_range] - \
-            self.df['close'].values[step_range] < 0
-        neg = self.df['open'].values[step_range] - \
-            self.df['close'].values[step_range] > 0
+        pos1 = self.df1['open'].values[step_range] - \
+            self.df1['close'].values[step_range] < 0
+        neg1 = self.df1['open'].values[step_range] - \
+            self.df1['close'].values[step_range] > 0
+        pos2 = self.df2['open'].values[step_range] - \
+            self.df2['close'].values[step_range] < 0
+        neg2 = self.df2['open'].values[step_range] - \
+            self.df2['close'].values[step_range] > 0
+        pos3 = self.df3['open'].values[step_range] - \
+            self.df3['close'].values[step_range] < 0
+        neg3 = self.df3['open'].values[step_range] - \
+            self.df3['close'].values[step_range] > 0
 
         # Color volume bars based on price direction on that date
-        self.volume_ax.bar(dates[pos], volume[pos], color=UP_COLOR,
+        self.volume_ax1.bar(dates[pos1], volume1[pos1], color=UP_COLOR,
                            alpha=0.4, width=.04, align='center')
-        self.volume_ax.bar(dates[neg], volume[neg], color=DOWN_COLOR,
+        self.volume_ax1.bar(dates[neg1], volume1[neg1], color=DOWN_COLOR,
+                           alpha=0.4, width=.04, align='center')
+        self.volume_ax2.bar(dates[pos2], volume2[pos2], color=UP_COLOR,
+                           alpha=0.4, width=.04, align='center')
+        self.volume_ax2.bar(dates[neg2], volume2[neg2], color=DOWN_COLOR,
+                           alpha=0.4, width=.04, align='center')
+        self.volume_ax3.bar(dates[pos3], volume3[pos3], color=UP_COLOR,
+                           alpha=0.4, width=.04, align='center')
+        self.volume_ax3.bar(dates[neg3], volume3[neg3], color=DOWN_COLOR,
                            alpha=0.4, width=.04, align='center')
 
         # Cap volume axis height below price chart and hide ticks
-        self.volume_ax.set_ylim(0, max(volume) / VOLUME_CHART_HEIGHT)
-        self.volume_ax.yaxis.set_ticks([])
+        self.volume_ax1.set_ylim(0, max(volume1) / VOLUME_CHART_HEIGHT)
+        self.volume_ax1.yaxis.set_ticks([])
+        self.volume_ax2.set_ylim(0, max(volume2) / VOLUME_CHART_HEIGHT)
+        self.volume_ax2.yaxis.set_ticks([])
+        self.volume_ax3.set_ylim(0, max(volume3) / VOLUME_CHART_HEIGHT)
+        self.volume_ax3.yaxis.set_ticks([])
 
-    def _render_trades(self, current_step, trades, step_range):
-        for trade in trades:
+    def _render_trades(self, current_step, trades1, trades2, trades3, step_range):
+        for trade in trades1:
             if trade['step'] in step_range:
-                date = date2num(self.df['Date'].values[trade['step']])
-                high = self.df['high'].values[trade['step']]
-                low = self.df['low'].values[trade['step']]
+                date = date2num(self.df1['Date'].values[trade['step']])
+                high = self.df1['high'].values[trade['step']]
+                low = self.df1['low'].values[trade['step']]
 
                 if trade['type'] == 'buy':
                     high_low = low
@@ -175,16 +233,70 @@ class StockTradingGraph:
                 total = '{0:.5f}'.format(trade['total'])
 
                 # print icon
-                self.price_ax.scatter(date, high_low, color=color, marker=marker, s=50)
+                self.price_ax1.scatter(date, high_low, color=color, marker=marker, s=50)
 
                 # Print the current price to the price axis
-                self.price_ax.annotate('{} {}'.format(total, self.second_coin),
+                self.price_ax1.annotate('{} {}'.format(total, self.second_coin),
                                        xy=(date, high_low),
                                        xytext=(date, high_low),
                                        color=color,
                                        fontsize=8)
 
-    def render(self, current_step, net_worth, buy_and_hold, trades, shares_held1, shares_held2, shares_held3, balance, window_size):
+        for trade in trades2:
+            if trade['step'] in step_range:
+                date = date2num(self.df1['Date'].values[trade['step']])
+                high = self.df2['high'].values[trade['step']]
+                low = self.df2['low'].values[trade['step']]
+
+                if trade['type'] == 'buy':
+                    high_low = low
+                    color = UP_TEXT_COLOR
+                    marker = '^'
+                else:
+                    high_low = high
+                    color = DOWN_TEXT_COLOR
+                    marker = 'v'
+
+                total = '{0:.5f}'.format(trade['total'])
+
+                # print icon
+                self.price_ax2.scatter(date, high_low, color=color, marker=marker, s=50)
+
+                # Print the current price to the price axis
+                self.price_ax2.annotate('{} {}'.format(total, self.second_coin),
+                                       xy=(date, high_low),
+                                       xytext=(date, high_low),
+                                       color=color,
+                                       fontsize=8)
+
+        for trade in trades3:
+            if trade['step'] in step_range:
+                date = date2num(self.df1['Date'].values[trade['step']])
+                high = self.df3['high'].values[trade['step']]
+                low = self.df3['low'].values[trade['step']]
+
+                if trade['type'] == 'buy':
+                    high_low = low
+                    color = UP_TEXT_COLOR
+                    marker = '^'
+                else:
+                    high_low = high
+                    color = DOWN_TEXT_COLOR
+                    marker = 'v'
+
+                total = '{0:.5f}'.format(trade['total'])
+
+                # print icon
+                self.price_ax3.scatter(date, high_low, color=color, marker=marker, s=50)
+
+                # Print the current price to the price axis
+                self.price_ax3.annotate('{} {}'.format(total, self.second_coin),
+                                       xy=(date, high_low),
+                                       xytext=(date, high_low),
+                                       color=color,
+                                       fontsize=8)
+
+    def render(self, current_step, net_worth, buy_and_hold, trades1, trades2, trades3, shares_held1, shares_held2, shares_held3, balance, window_size):
         self.net_worths[current_step] = net_worth
         self.buy_and_holds[current_step] = buy_and_hold
 
@@ -193,15 +305,19 @@ class StockTradingGraph:
 
         # Format dates as timestamps, necessary for candlestick graph
         dates = np.array([date2num(x)
-                          for x in self.df['Date'].values[step_range]])
+                          for x in self.df1['Date'].values[step_range]])
 
         self._render_net_worth(current_step, net_worth, buy_and_hold, shares_held1, shares_held2, shares_held3, balance, step_range, dates)
         self._render_price(current_step, net_worth, dates, step_range)
         self._render_volume(current_step, net_worth, dates, step_range)
-        self._render_trades(current_step, trades, step_range)
+        self._render_trades(current_step, trades1, trades2, trades3, step_range)
 
         # Format the date ticks to be more easily read
-        self.price_ax.set_xticklabels(self.df['Date'].values[step_range], rotation=45,
+        self.price_ax1.set_xticklabels(self.df1['Date'].values[step_range], rotation=45,
+                                      horizontalalignment='right')
+        self.price_ax2.set_xticklabels(self.df2['Date'].values[step_range], rotation=45,
+                                      horizontalalignment='right')
+        self.price_ax3.set_xticklabels(self.df3['Date'].values[step_range], rotation=45,
                                       horizontalalignment='right')
 
         # Hide duplicate net worth date labels
