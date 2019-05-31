299a79b38da8117799923ae30fcd54290d604526
draichi <lucasdraichi@gmail.com>
2019-05-31 10:26:56 -0300

    updating vars

--- configs/vars.py
+++ configs/vars.py
@@ -9,17 +9,17 @@ DF_TRAIN_SIZE = 0.75
 ############ RENDER VARIABLES ################
 VOLUME_CHART_HEIGHT = 0.33
 
-# style 1
-# UP_COLOR = '#297fff'
-# DOWN_COLOR = '#ffaa00'
-# UP_TEXT_COLOR = '#297fff'
-# DOWN_TEXT_COLOR = '#ffaa00'
+CANDLESTICK_WIDTH = {
+    "day": 1,
+    "hour": 0.4,
+    "minute": 0.0006
+}
+BALANCE_COLOR = ['#ffa929', '#ff297f', '#7fff29', '#297fff']
 
-# style 2
 UP_COLOR = '#00b909'
 DOWN_COLOR = '#c60606'
 UP_TEXT_COLOR = '#00b909'
 DOWN_TEXT_COLOR = '#c60606'
 
-BOT_COLOR = '#ffffff'
-BUY_N_HOLD_COLOR = '#dcaba2'
+BUY_N_HOLD_COLOR = '#297fff'
+BOT_COLOR = '#ffaa00'
