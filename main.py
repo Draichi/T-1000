5b37b685e86513f563ec8b5d20cd5e84b1c43e3d
draichi <lucasdraichi@gmail.com>
2020-09-15 16:58:14 -0300

    refactor: adds train/test option

--- main.py
+++ main.py
@@ -20,28 +20,31 @@ if __name__ == '__main__':
 	from utils import loading
 	loading()
 	from t_1000.application import T1000
+
 	env = T1000(assets=['XRP','BCH','LTC','BNB'],
                       currency='BTC',
                       granularity='day',
                       datapoints=300)
+	if not args.checkpoint_path: # train
+		env.train(timesteps=3e5,
+		          checkpoint_freq=10,
+		          lr_schedule=[
+		              [
+		                  [0, 7e-5],  # [timestep, lr]
+		                  [1e6, 7e-6],
+		              ],
+		            #   [
+		            #       [0, 6e-5],
+		            #       [1e6, 6e-6],
+		            #   ],
+		            #   [
+		            #       [0, 5e-5],
+		            #       [1e6, 5e-6],
+		            #   ]
+		          ],
+		          algo='PPO')
+	else: # test
+		checkpoint_path = 'results/t-1000/1_2020-09-03_07-41-091y8hnjtt/checkpoint_10/checkpoint-10'
+		env.backtest(checkpoint_path=checkpoint_path)
 
-    # env.train(timesteps=3e5,
-    #           checkpoint_freq=10,
-    #           lr_schedule=[
-    #               [
-    #                   [0, 7e-5],  # [timestep, lr]
-    #                   [1e6, 7e-6],
-    #               ],
-    #             #   [
-    #             #       [0, 6e-5],
-    #             #       [1e6, 6e-6],
-    #             #   ],
-    #             #   [
-    #             #       [0, 5e-5],
-    #             #       [1e6, 5e-6],
-    #             #   ]
-    #           ],
-    #           algo='PPO')
               
-	checkpoint_path = 'results/t-1000/1_2020-09-03_07-41-091y8hnjtt/checkpoint_10/checkpoint-10'
-	env.backtest(checkpoint_path=checkpoint_path)
