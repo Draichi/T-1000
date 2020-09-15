805614e087464e9824e5bcafb56bec5a5d819ada
draichi <lucasdraichi@gmail.com>
2020-09-15 17:09:52 -0300

    refactor: adds __init__ file to render

--- t_1000/env/trading_env.py
+++ t_1000/env/trading_env.py
@@ -3,8 +3,7 @@ import gym
 from gym import spaces
 import numpy as np
 import colorama
-# from core_render import GraphGenerator
-from t_1000.render.graph_generator import GraphGenerator
+from t_1000.render import GraphGenerator
 
 colorama.init()
 WINDOW_SIZE = 40
